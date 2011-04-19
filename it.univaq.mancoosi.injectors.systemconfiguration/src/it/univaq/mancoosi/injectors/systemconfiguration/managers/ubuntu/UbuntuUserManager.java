a99173410deedde5e41889fea0444e9d7caae085
mircofranzago@gmail.com <mircofranzago@gmail.com@c8f2f23f-27b7-32fa-be4b-d2bc113dbd9a>
2011-04-19 16:53:13 +0000

    UbuntuUserManager improvement and some other minor improvements

--- it.univaq.mancoosi.injectors.systemconfiguration/src/it/univaq/mancoosi/injectors/systemconfiguration/managers/ubuntu/UbuntuUserManager.java
+++ it.univaq.mancoosi.injectors.systemconfiguration/src/it/univaq/mancoosi/injectors/systemconfiguration/managers/ubuntu/UbuntuUserManager.java
@@ -1,27 +1,30 @@
 package it.univaq.mancoosi.injectors.systemconfiguration.managers.ubuntu;
 
 
+import it.univaq.mancoosi.injectors.systemconfiguration.managers.UserManager;
 import java.io.IOException;
 
-import it.univaq.mancoosi.injectors.systemconfiguration.managers.UserManager;
-
 public class UbuntuUserManager extends UserManager {
-	
+
 	protected UbuntuUserManager(){
-			this.configuration = UbuntuConfigurationManager.getInstance().getConfiguration();
-			this.groupManager = UbuntuGroupManager.getInstance();
-		}
-	
+		this.configuration = UbuntuConfigurationManager.getInstance().getConfiguration();
+		this.groupManager = UbuntuGroupManager.getInstance();
+	}
+
 	public static UserManager getInstance(){
 		if (INSTANCE == null)
 			INSTANCE = new UbuntuUserManager();
 		return INSTANCE;
 	}
 
+	//mirco
 	@Override
 	public void synchronize() throws IOException {
-		// TODO Auto-generated method stub
-		
+
+		configuration.getEnvironment().getUsers().clear();
+		configuration.getEnvironment().getGroups().clear();
+
+		UserManager.getInstance().createUsersFromSystem();
 	}
-	 
+
 }
