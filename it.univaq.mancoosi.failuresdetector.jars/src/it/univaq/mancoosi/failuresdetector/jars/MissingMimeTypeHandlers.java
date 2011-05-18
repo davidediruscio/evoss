bff331cbd8e6aeba1d540d1f325604acae47fffa
davide.diruscio@gmail.com <davide.diruscio@gmail.com@c8f2f23f-27b7-32fa-be4b-d2bc113dbd9a>
2011-05-18 11:01:15 +0000

    Some more JARQueries

---
+++ it.univaq.mancoosi.failuresdetector.jars/src/it/univaq/mancoosi/failuresdetector/jars/MissingMimeTypeHandlers.java
@@ -0,0 +1,53 @@
+package it.univaq.mancoosi.failuresdetector.jars;
+
+
+import java.io.IOException;
+import java.util.Iterator;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
+import it.univaq.mancoosi.mancoosimm.Configuration;
+import it.univaq.mancoosi.mancoosimm.MimeTypeHandler;
+import it.univaq.mancoosi.mancoosimm.impl.MancoosiPackageImpl;
+import it.univaq.mancoosi.injectors.systemconfiguration.managers.ubuntu.UbuntuConfigurationManager;
+
+
+public class MissingMimeTypeHandlers {
+	
+	static 	Configuration configuration = null;
+	
+	public static void checkMissingMimeTypeHandlers() {
+		
+		System.out.println("Cheking for missing MimeTypeHandlers....");
+		
+		Iterator<MimeTypeHandler> mthit = configuration.getEnvironment().getMimeTypeHandlerCache().getHandlers().iterator();
+		
+		MimeTypeHandler mth = null;
+		java.io.File aux = null;
+		
+		while ( mthit.hasNext() ) {
+			
+			mth = mthit.next();
+			aux = new java.io.File(mth.getHandler().getLocation());
+			if ( ! aux.exists() ) {
+				System.out.println("WARNING: L'handler " +mth.getHandler().getLocation() + " is missing and it is necessary to manage the "  + mth.getType().getName() + " MimeType");
+			}
+			
+		}
+		System.out.println("Cheking for missing MimeTypeHandlers....DONE");
+		
+	}
+	
+	
+	public static void main(String[] args) throws IOException{
+		MancoosiPackageImpl.init();
+		
+		URI fileURI = URI.createFileURI("model/systemModel.ecore");
+		Resource resource = new XMIResourceFactoryImpl().createResource(fileURI);
+		resource.load(null);
+		UbuntuConfigurationManager.getInstance().setConfiguration( (Configuration) resource.getContents().get(0) );
+		configuration = UbuntuConfigurationManager.getInstance().getConfiguration();
+		
+		checkMissingMimeTypeHandlers();
+	}
+}
